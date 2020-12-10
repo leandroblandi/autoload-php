1bcfae7e881fdaecb8022b3d3dd64842d80c8a1a
Leandro Blandi <59542421+leandroblandi@users.noreply.github.com>
2020-12-09 21:09:08 -0300

    Add files via upload

---
+++ autoload.php
@@ -0,0 +1,17 @@
+﻿<?php
+	class CargadorClases {
+		# reemplazar por carpeta necesaria
+		public static string $carpeta = "App/Controllers/";
+		public static function incluir($clase)
+		{
+			include self::$carpetaClases.$clase;
+		}
+		public static function cargarClases()
+		{
+			return spl_autoload_register("self::incluir");
+		}
+	}
+	# con este metodo estatico cargamos todas las clases
+	# que nos dice la propiedad $carpeta
+	CargadorClases::cargarClases();
+?>
